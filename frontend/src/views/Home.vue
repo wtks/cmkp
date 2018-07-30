7e26a589f17ba81ff8c6e7c73a6bc5dfe677ddbd
takashi <t.k.mk3.110@gmail.com>
2018-07-30 18:09:05 +0900

    home

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -9,14 +9,19 @@
             span リクエストをし終わったら、必ず「マイリクエスト」の「希望優先順位を設定する」から特に優先して買ってきて欲しいサークルを、日程毎に設定してください。
             br
             span リクエストに関して管理人に伝えたいことがある場合は、メニューの「リクエスト備考」からリクエスト備考を書くことができます。
+            br
+            span リクエスト受付締め切り後は、リクエストの登録・削除・個数の変更・優先順位設定は出来ません。何かあれば管理人に個別に連絡してください。
       v-flex
         v-card
           v-card-title リクエスト受付締め切り日時
           v-card-text
-            p 企業: {{ enterpriseDeadline.format('MM/DD HH:mm') }}
-            p 1日目: {{ day1Deadline.format('MM/DD HH:mm') }}
-            p 2日目: {{ day2Deadline.format('MM/DD HH:mm') }}
-            p 3日目: {{ day3Deadline.format('MM/DD HH:mm') }}
+            span 企業: {{ enterpriseDeadline.format('MM/DD HH:mm') }}
+            br
+            span 1日目: {{ day1Deadline.format('MM/DD HH:mm') }}
+            br
+            span 2日目: {{ day2Deadline.format('MM/DD HH:mm') }}
+            br
+            span 3日目: {{ day3Deadline.format('MM/DD HH:mm') }}
 
 </template>
 
